7146ad51b0c9a113a90c7edd5277708408965e32
Wesley Luk <Wesley Luk>
2016-01-10 15:40:52 -0800

    Added log to gitignore, fixing tsd

--- dist/typescript-injector.d.ts
+++ dist/typescript-injector.d.ts
@@ -1,15 +1,21 @@
-export interface TConstructor<T> {
-    new (...args: any[]): T;
-    inject?: string[];
-    name?: string;
+declare namespace TypescriptInjector {
+    export interface TConstructor<T> {
+        new (...args: any[]): T;
+        inject?: string[];
+        name?: string;
+    }
+    export class Injector {
+        private instanceLookup;
+        private classLookup;
+        constructor();
+        registerClass<T>(dependency: string, klass: TConstructor<T>): void;
+        registerInstance<T>(dependency: string, instance: T): void;
+        private assertNotRegistered(dependency);
+        resolve<T>(dependency: string, trail?: string[]): T;
+        private buildClass<T>(klass, dependencies);
+    }
 }
-export declare class Injector {
-    private instanceLookup;
-    private classLookup;
-    constructor();
-    registerClass<T>(dependency: string, klass: TConstructor<T>): void;
-    registerInstance<T>(dependency: string, instance: T): void;
-    private assertNotRegistered(dependency);
-    resolve<T>(dependency: string, trail?: string[]): T;
-    private buildClass<T>(klass, dependencies);
+
+declare module "typescript-injector" {
+    export = TypescriptInjector;
 }
